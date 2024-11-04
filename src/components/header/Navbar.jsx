c990153008b7c7146ed1e212f62c073459c531da
Abdullah <graphicdesigner.abdullah@gmail.com>
2024-11-05 01:26:36 +0600

    Updated

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,10 +1,13 @@
 import { useLocation } from "react-router-dom";
 import NavList from "./NavList";
 import { FaShoppingCart, FaHeart } from "react-icons/fa";
+import { getFromLS } from "../../utils/LocalStorage";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
   const location = useLocation();
   const pathName = location.pathname;
+  const [iconV, setIconV] = useState({ cart: 0, wish: 0 });
 
   const navbar = [
     {
@@ -28,6 +31,17 @@ const Navbar = () => {
       path: "/blogs",
     },
   ];
+
+  const handleIconV = () => {
+    const cart = getFromLS("cart");
+    const wish = getFromLS("wish");
+    setIconV({ cart: cart.length, wish: wish.length });
+    console.log('ami');
+  };
+
+  useEffect(() => {
+    handleIconV();
+  }, []);
 
   return (
     <nav
@@ -54,12 +68,22 @@ const Navbar = () => {
         </ul>
 
         <div className="flex items-center gap-x-6">
-          <button className="bg-zinc-100 p-2 rounded-full text-xl">
-            <FaShoppingCart />
-          </button>
-          <button className="bg-zinc-100 p-2 rounded-full text-xl">
-            <FaHeart />
-          </button>
+          <div className="relative">
+            <button className="bg-zinc-100 p-2 rounded-full text-xl">
+              <FaShoppingCart />
+            </button>
+            <span className="absolute -top-5 left-7 bg-white py-1 px-2 text-red-500 rounded-full">
+              {iconV.cart}
+            </span>
+          </div>
+          <div className="relative">
+            <button className="bg-zinc-100 p-2 rounded-full text-xl">
+              <FaHeart />
+            </button>
+            <span className="absolute -top-5 left-7 bg-white py-1 px-2 text-red-500 rounded-full">
+              {iconV.wish}
+            </span>
+          </div>
         </div>
       </div>
     </nav>
--- src/components/pages/dashboard/CartList.jsx
+++ src/components/pages/dashboard/CartList.jsx
@@ -2,42 +2,60 @@ import PropTypes from "prop-types";
 import { clearFromLS, getFromLS } from "../../../utils/LocalStorage";
 import { useEffect, useState } from "react";
 import CartCard from "./CartCard";
+import { BsSortNumericDownAlt } from "react-icons/bs";
 
 const CartList = ({ data }) => {
   const [filteredData, setFilteredData] = useState([]);
+  const [totalCost, setTotalCost] = useState(0);
   const storedData = getFromLS("Cart");
 
   const loadFromLS = () => {
     let storedC_Data = [];
+    let cost = 0;
 
     for (const item of storedData) {
       data.map((i) => {
-        i.productID == item ? storedC_Data.push(i) : "";
+        if (i.productID == item) {
+          storedC_Data.push(i);
+          cost += i.price;
+        }
       });
     }
     setFilteredData(storedC_Data);
+    setTotalCost(cost);
   };
 
   useEffect(() => {
     loadFromLS();
   }, [data]);
 
+  const handleSortByPrice = () => {
+    const sortedData = [...filteredData].sort((a, b) => b.price - a.price);
+    setFilteredData(sortedData);
+  };
+
   const handleClearFromLS = (item, keyName) => {
     clearFromLS(item, keyName);
 
     const newData = filteredData.filter((data) => data.productID !== item);
     setFilteredData(newData);
+
+    let cost = 0;
+    newData.map((data) => (cost += data.price));
+    setTotalCost(cost);
   };
 
   return (
     <section className="px-5">
       <div className="max-w-7xl mx-auto">
         <div className="flex flex-col lg:flex-row gap-5 justify-between mt-8">
-          <h3>Cart</h3>
+          <h3 className="text-xl font-semibold">Cart</h3>
           <div className="flex items-center gap-4">
-            <p className="font-semibold">Total cost:</p>
-            <button className="py-2 px-6 border-2 border-primary rounded-full">
-              Sort by price
+            <p className="font-semibold">Total cost: ${totalCost} </p>
+            <button
+            onClick={handleSortByPrice}
+            className="py-2 px-6 border-2 border-primary rounded-full flex items-center gap-2">
+              Sort by price <BsSortNumericDownAlt/>
             </button>
             <button className="py-2 px-6 border-2 border-primary rounded-full bg-primary text-white">
               Purchase
