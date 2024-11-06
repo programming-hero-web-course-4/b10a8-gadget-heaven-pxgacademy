9f3c5212ed30175da0c9122e1ef8a52f58a14731
Abdullah <graphicdesigner.abdullah@gmail.com>
2024-11-06 13:27:38 +0600

    Updated

--- src/components/pages/dashboard/CartCard.jsx
+++ src/components/pages/dashboard/CartCard.jsx
@@ -12,8 +12,8 @@ const CartCard = ({ item, handleClearFromLS }) => {
       <div className="md:max-w-60 w-full mb-6 md:mb-0">
         <img className="w-full rounded-xl" src={image} alt="" />
       </div>
-      <div className="flex gap-x-7 justify-between items-center flex-grow md:ml-6">
-        <div className="space-y-2">
+      <div className="flex flex-col md:flex-row w-full gap-x-7 justify-between items-center flex-grow md:ml-6">
+        <div className="space-y-2 w-full">
           <h3 className="text-2xl font-semibold">{productName}</h3>
           <p className="text-zinc-500">{description}</p>
           <p className="font-semibold text-lg">${price}</p>
@@ -23,7 +23,7 @@ const CartCard = ({ item, handleClearFromLS }) => {
             onClick={() => {
               handleClearFromLS(productID, "Cart");
               setCartLength(cartLength - 1);
-              toast.error('Remove the product from the cart')
+              toast.error("Remove the product from the cart");
             }}
             className="text-xl text-red-500 border-2 border-red-500 rounded-full p-2"
           >
--- src/components/pages/dashboard/WishCard.jsx
+++ src/components/pages/dashboard/WishCard.jsx
@@ -13,34 +13,34 @@ const WishCard = ({ item, handleClearFromLS }) => {
       <div className="md:max-w-60 w-full mb-6 md:mb-0">
         <img className="w-full rounded-xl" src={image} alt="" />
       </div>
-      <div className="flex gap-x-7 justify-between items-center flex-grow md:ml-6">
-        <div className="space-y-2">
+      <div className="flex-grow md:ml-6 w-full">
+        <div className="space-y-2 ">
           <h3 className="text-2xl font-semibold">{productName}</h3>
           <p className="text-zinc-500">{description}</p>
           <p className="font-semibold text-lg">${price}</p>
-          <button
-            onClick={() => {
-              setWishLength(wishLength - 1);
-              handleClearFromLS(productID, "Wish");
-              setToLS(productID, "Cart");
-              toast.success("Product successfully added to Cart");
-            }}
-            className="bg-primary text-white py-1 px-4 rounded"
-          >
-            Add to cart
-          </button>
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              setWishLength(wishLength - 1);
-              handleClearFromLS(productID, "Wish");
-              toast.error("Remove the product from the Wishlist");
-            }}
-            className="text-xl text-red-500 border-2 border-red-500 rounded-full p-2"
-          >
-            <IoClose />
-          </button>
+          <div className="flex items-center justify-between">
+            <button
+              onClick={() => {
+                setWishLength(wishLength - 1);
+                handleClearFromLS(productID, "Wish");
+                setToLS(productID, "Cart");
+                toast.success("Product successfully added to Cart");
+              }}
+              className="bg-primary text-white py-1 px-4 rounded"
+            >
+              Add to cart
+            </button>
+            <button
+              onClick={() => {
+                setWishLength(wishLength - 1);
+                handleClearFromLS(productID, "Wish");
+                toast.error("Remove the product from the Wishlist");
+              }}
+              className="text-xl text-red-500 border-2 border-red-500 rounded-full p-2"
+            >
+              <IoClose />
+            </button>
+          </div>
         </div>
       </div>
     </div>
